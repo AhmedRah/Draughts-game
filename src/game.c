fb22514192bd1de37a4709a16bd729daff42cdea
AhmedRah <ahmedokestudent@gmail.com>
2020-10-31 15:31:52 +0100

    Finally finihsed Game.c file
    fninished te create and load finctions for game

--- src/game.c
+++ src/game.c
@@ -32,7 +32,7 @@
 int create_game()
 {
   // Creer une table de jeux vide
-  pieces Arr_Table[XSIZE_TABLE][YSIZE_TABLE] = {0,0};
+  pieces Arr_Table[XSIZE_TABLE][YSIZE_TABLE] = {0};
   // Initialise les joueurs 
   pieces joueur1 = {1,0};
   pieces joueur2 = {2,0}; 
@@ -51,7 +51,7 @@ int create_game()
       }
     }
   } 
-  FILE* file = fopen("game.txt","a+");
+  FILE* file = fopen("gameTable.txt","w");
   for (int j = 0; j < YSIZE_TABLE; j++)
   {
     for (int i = 0; i < XSIZE_TABLE; i++)
@@ -60,45 +60,46 @@ int create_game()
     }
   }
   fclose(file);
-
-  for (int j = 0; j < YSIZE_TABLE; j++)
-  {
-    for (int i = 0; i < XSIZE_TABLE; i++)
-    {
-      printf("%d ",Arr_Table[i][j].team);
-    }
-  }
   return 0;
 }
-int load_game(pieces* Arr_Table[XSIZE_TABLE][YSIZE_TABLE])
+int load_game(pieces Arr_Table[XSIZE_TABLE][YSIZE_TABLE])
 {
-  pieces* joueur1 = (pieces*)(1*sizeof(pieces));
-  pieces* joueur2 = (pieces*)(1*sizeof(pieces));
-  joueur1->team = 1;joueur1->team = 0;
-  joueur2->team = 1;joueur2->team = 0;
+  // pieces* empty = malloc(1*sizeof(pieces));
+  // pieces* joueur1 = malloc(1*sizeof(pieces));
+  // pieces* joueur2 = malloc(1*sizeof(pieces));
+  // joueur1->team = 1;joueur1->statut = 0;
+  // joueur2->team = 2;joueur2->statut = 0;
+  // empty->team   = 0;empty->statut   = 0;
   // charger le table de jeux
   char fileline[XSIZE_TABLE*YSIZE_TABLE]  = {0};
   
-  FILE *file = fopen("game.txt","r");
+  FILE *file = fopen("gameTable.txt","r");
   if(file == NULL)
     return 0;
   fgets(fileline,XSIZE_TABLE*YSIZE_TABLE,file);
   
-  for(int y=0;y<YSIZE_TABLE;y++)
+  for(int y=0;y<YSIZE_TABLE; y++)
   {
     for (int x = 0; x < XSIZE_TABLE; x++)
     {
       switch (fileline[y*YSIZE_TABLE + x])
       {
+        case '0':
+          Arr_Table[x][y].team = 0;
+          Arr_Table[x][y].statut = 0;
+          break;
         case '1':
-          Arr_Table[x][y] = joueur1;
+          Arr_Table[x][y].team = 1;
+          Arr_Table[x][y].statut = 0;
           break;
         case '2':
-          Arr_Table[x][y] = joueur2;
+          Arr_Table[x][y].team = 2;
+          Arr_Table[x][y].statut = 0;
           break;  
       }
     }
   }
+
   fclose(file);
   return 1;
 } 
@@ -107,16 +108,10 @@ int main()
 {
   create_game();
 
-  // // Creer une table de jeux vide
-  pieces *Arr_Table[XSIZE_TABLE][YSIZE_TABLE] = {0,0};
+  // Creer une table de jeux vide
+  pieces Arr_Table[XSIZE_TABLE][YSIZE_TABLE]={0};
+
   load_game(Arr_Table);
   
-  // for (int j = 0; j < YSIZE_TABLE; j++)
-  // {
-  //   for (int i = 0; i < XSIZE_TABLE; i++)
-  //   {
-  //     printf("%d ",Arr_Table[i][j]->team);
-  //   }
-  // }
   return EXIT_SUCCESS;
 }
