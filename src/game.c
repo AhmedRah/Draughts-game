2a2046c8824a3e07d42f4c399935643c274b7a81
AhmedRah <ahmedokestudent@gmail.com>
2020-10-31 11:39:26 +0100

    removed SDL For now
    trying to figure out the game mechanics

--- src/game.c
+++ src/game.c
@@ -10,35 +10,33 @@
 #include <errno.h>
 
 // #include "../include/SDL.h"
-#include "../include/SDL_net.h"
-#include <SDL2/SDL.h>
-
-
+// #include <SDL2/SDL.h>
+// #include <SDL2/SDL_image.h>
+// #include <SDL2/SDL_net.h>
 #include "headerFile.h"
 
-typedef struct Piece {
-  int team ; //l'equipe : 1 pour premier joueur  , 2 pour deuxieme joueur.
-  int statut ; // 0 pour piece normale , 1 pour hors-jeux , 2 pour dame
-} pieces;
+// typedef struct Piece {
+//   int team ; //l'equipe : 1 pour premier joueur  , 2 pour deuxieme joueur.
+//   int statut ; // 0 pour piece normale , 1 pour hors-jeux , 2 pour dame
+// } pieces;
 
 
-typedef struct Games
-{
-  pieces* Arr_Table ; // Tableaux de jeux
-  pieces* Arr_pieces ;// Tableaux des peices
-} game ;
+// typedef struct Games
+// {
+//   pieces* Arr_Table ; // Tableaux de jeux
+//   pieces* Arr_pieces ;// Tableaux des peices
+// } game ;
 
 //
 
-void play()
+int create_game()
 {
   // Creer une table de jeux vide
   pieces Arr_Table[XSIZE_TABLE][YSIZE_TABLE] = {0,0};
   // Initialise les joueurs 
-  pieces joueur1 ={1,0};
-  pieces joueur2 ={2,0}; 
+  pieces joueur1 = {1,0};
+  pieces joueur2 = {2,0}; 
 
-  
   for(int y=0;y<YSIZE_TABLE;y++)
   {
     //positionner les cases des joueurs 1 et 2
@@ -53,6 +51,15 @@ void play()
       }
     }
   } 
+  FILE* file = fopen("game.txt","a+");
+  for (int j = 0; j < YSIZE_TABLE; j++)
+  {
+    for (int i = 0; i < XSIZE_TABLE; i++)
+    {
+      fprintf(file,"%d",Arr_Table[i][j].team);
+    }
+  }
+  fclose(file);
 
   for (int j = 0; j < YSIZE_TABLE; j++)
   {
@@ -60,81 +67,56 @@ void play()
     {
       printf("%d ",Arr_Table[i][j].team);
     }
-    printf("\n");
   }
+  return 0;
+}
+int load_game(pieces* Arr_Table[XSIZE_TABLE][YSIZE_TABLE])
+{
+  pieces* joueur1 = (pieces*)(1*sizeof(pieces));
+  pieces* joueur2 = (pieces*)(1*sizeof(pieces));
+  joueur1->team = 1;joueur1->team = 0;
+  joueur2->team = 1;joueur2->team = 0;
+  // charger le table de jeux
+  char fileline[XSIZE_TABLE*YSIZE_TABLE]  = {0};
   
+  FILE *file = fopen("game.txt","r");
+  if(file == NULL)
+    return 0;
+  fgets(fileline,XSIZE_TABLE*YSIZE_TABLE,file);
+  
+  for(int y=0;y<YSIZE_TABLE;y++)
+  {
+    for (int x = 0; x < XSIZE_TABLE; x++)
+    {
+      switch (fileline[y*YSIZE_TABLE + x])
+      {
+        case '1':
+          Arr_Table[x][y] = joueur1;
+          break;
+        case '2':
+          Arr_Table[x][y] = joueur2;
+          break;  
+      }
+    }
+  }
+  fclose(file);
+  return 1;
 } 
 
-int main(void)
+int main()
 {
-  TCPsocket sd, csd; 
-  /* Socket descriptor, Client socket descriptor */ 
-  IPaddress ip, *remoteIP; 
-  int quit, quit2; 
-  char buffer[512];
+  create_game();
 
-  if (SDLNet_Init() < 0) 
-  { 
-    fprintf(stderr, "SDLNet_Init: %s\n", SDLNet_GetError()); exit(EXIT_FAILURE); 
-  }
-
-  /* Resolving the host using NULL make network interface to listen */ 
-  if (SDLNet_ResolveHost(&ip, NULL, 2000) < 0) 
-  {
-    fprintf(stderr, "SDLNet_ResolveHost: %s\n", SDLNet_GetError()); exit(EXIT_FAILURE); 
-  }
-
-  /* Open a connection with the IP provided (listen on the host's port) */ 
-  if (!(sd = SDLNet_TCP_Open(&ip))) 
-  { 
-    fprintf(stderr, "SDLNet_TCP_Open: %s\n", SDLNet_GetError())
-    ; exit(EXIT_FAILURE); 
-  }
-
-  /* Wait for a connection, send data and term */ 
-  quit = 0; 
-  while (!quit) 
-  { /* This check the sd if there is a pending connection. * If there is one, accept that, and open a new socket for communicating */ 
-    if ((csd = SDLNet_TCP_Accept(sd))) 
-    { 
-      /* Now we can communicate with the client using csd socket * sd will remain opened waiting other connections */
-
-      /* Get the remote address */ 
-      if ((remoteIP = SDLNet_TCP_GetPeerAddress(csd))) 
-        /* Print the address, converting in the host format */ 
-        printf("Host connected: %x %d\n", SDLNet_Read32(&remoteIP->host), SDLNet_Read16(&remoteIP->port)); 
-      else 
-        fprintf(stderr, "SDLNet_TCP_GetPeerAddress: %s\n", SDLNet_GetError());
-
-      quit2 = 0; 
-      while (!quit2) 
-      { 
-        if (SDLNet_TCP_Recv(csd, buffer, 512) > 0) 
-        { 
-          printf("Client say: %s\n", buffer);
-
-          if(strcmp(buffer, "exit") == 0) 
-          /* Terminate this connection */ 
-          { 
-            quit2 = 1; printf("Terminate connection\n"); 
-          } 
-          if(strcmp(buffer, "quit") == 0) 
-          /* Quit the program */ 
-          { 
-            quit2 = 1; 
-            quit = 1; 
-            printf("Quit program\n"); 
-          } 
-        } 
-      }
-
-      /* Close the client socket */ 
-      SDLNet_TCP_Close(csd);   
-    } 
-  }
-
-  SDLNet_TCP_Close(sd); 
-  SDLNet_Quit();
-
-  return EXIT_SUCCESS; 
+  // // Creer une table de jeux vide
+  pieces *Arr_Table[XSIZE_TABLE][YSIZE_TABLE] = {0,0};
+  load_game(Arr_Table);
+  
+  // for (int j = 0; j < YSIZE_TABLE; j++)
+  // {
+  //   for (int i = 0; i < XSIZE_TABLE; i++)
+  //   {
+  //     printf("%d ",Arr_Table[i][j]->team);
+  //   }
+  // }
+  return EXIT_SUCCESS;
 }
--- src/headerFile.h
+++ src/headerFile.h
@@ -10,4 +10,14 @@
   #define XSIZE_TABLE 10
   #define YSIZE_TABLE 10
 
+  #ifndef SOME_HEADER_GUARD_WITH_UNIQUE_NAME
+  #define SOME_HEADER_GUARD_WITH_UNIQUE_NAME
+
+  typedef struct Piece {
+    int team ; //l'equipe : 1 pour premier joueur  , 2 pour deuxieme joueur.
+    int statut ; // 0 pour piece normale , 1 pour hors-jeux , 2 pour dame
+  } pieces;
+
+  #endif
+
 #endif
